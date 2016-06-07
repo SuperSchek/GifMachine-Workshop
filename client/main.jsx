7b5160e3ec32735fcafc082cbc29544ef17e4113
Jacco Schumacher <jaccoschumacher@Jaccos-MacBook-Air.local>
2016-06-07 20:08:19 +0200

    style added

--- client/main.jsx
+++ client/main.jsx
@@ -1,10 +1,7 @@
 import { Template } from 'meteor/templating';
 import './main.html';
-import { ReactiveVar } from 'meteor/reactive-var';
-
 
 clickedImage = String;
-
 
 Meteor.startup(() => {
     GifBase = new Mongo.Collection('GifBase');
@@ -12,37 +9,35 @@ Meteor.startup(() => {
 });
 
 Template.gifDesktop.events({
-  'click img': function(event){
-    event.preventDefault();
-    clickedImage = this.url;
-    GifBase.update({_id: GifBase.findOne()._id}, {$set: {selectedImage: clickedImage}});
-    console.log(clickedImage);
-    console.log(this._id);
-  }
+    'click img': function(event){
+        event.preventDefault();
+        clickedImage = this.url;
+        GifBase.update({_id: GifBase.findOne()._id}, {$set: {selectedImage: clickedImage}});
+        console.log(clickedImage);
+        console.log(this._id);
+    }, 
+    'click a': function(event) {
+        event.preventDefault();
+        GifBase.remove({_id: this._id}, {url: this.url});
+    }
 });
+
 
 Template.desktop.events({
     'submit form': function(event){
         event.preventDefault();
-
         formUrl = document.getElementById('formUrl').value;
         GifBase.insert({url: formUrl, selectedImage: ""});
         console.log("√ Image uploaded.");
     }
 });
 
-Template.gifDesktop.events({
-  'click a': function(event) {
-    event.preventDefault();
-    GifBase.remove({_id: this._id}, {url: this.url});
-  }
-})
-
 Template.desktop.helpers({
   'gif': function () {
       return GifBase.find();
   }
 });
+
 
 Template.mobiel.helpers({
     'gifLink': function(){
